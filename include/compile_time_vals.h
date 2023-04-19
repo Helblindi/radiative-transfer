e4a1ad14066db4bc0dc41d6f5ec67d04f0cdd969
Madison Sheridan <madison.sheridan94@gmail.com>
2023-04-19 15:09:35 -0500

    Added additional quadrature order option.

--- include/compile_time_vals.h
+++ include/compile_time_vals.h
@@ -5,31 +5,35 @@ using namespace std;
 
 namespace rt
 {
-   const static int M = 2;                            // Quadrature order, even
+   const static int M = 2;                              // Quadrature order, even
    const double G_x[M] = {-0.5773502692, 0.5773502692}; // Quadrature points
-   const double G_w[M] = {6.283185307, 6.283185307};  // Quadrature weights, sum to 4Pi
-   const static double X = 2.;                        // Slab thickness
-   const static int N = 100;                          // Number of cells
-   const static double dx = X / N;                    // cell size
-   const static double a = .01372;                    // Radiation constant, jerks/ (cm^3/keV^4)
-   const static double c = 299.8;                     // Speed of light, cm/shake
-   const static int bc_left_indicator = 1;            // vacuum - 0, // TODO: Change this to an array that matches M / 2
-                                                      // source - 1, 
-                                                      // reflective - 2 
-   const static int bc_right_indicator = 1;           // vacuum - 0, // TODO: Change this to an array that matches M / 2
-                                                      // source - 1, 
-                                                      // reflective - 2 
+   const double G_w[M] = {2*M_PI, 2*M_PI};              // Quadrature weights, sum to 4Pi
+
+   // const static int M = 4;                              // Quadrature order, even
+   // const double G_x[M] = {-0.861136, -0.339981, 0.339981, 0.861136}; // Quadrature points
+   // const double G_w[M] = {2*M_PI*0.347855, 2*M_PI*0.652145, 2*M_PI*0.652145, 2*M_PI*0.347855};    // Quadrature weights, sum to 4Pi
+   
+   const static double X = 2.;                          // Slab thickness
+   const static int N = 100;                            // Number of cells
+   const static double dx = X / N;                      // cell size
+   const static double a = .01372;                      // Radiation constant, jerks/ (cm^3/keV^4)
+   const static double c = 299.8;                       // Speed of light, cm/shake
+   const static int bc_left_indicator = 2;              // vacuum - 0, // TODO: Change this to an array that matches M / 2
+                                                        // source - 1, 
+                                                        // reflective - 2 
+   const static int bc_right_indicator = 1;             // vacuum - 0, // TODO: Change this to an array that matches M / 2
+                                                        // source - 1, 
+                                                        // reflective - 2 
+
+   const static double rho = 1.;                        // Material density
+   const static double kappa = 1.;                      // Absorption opacity
+   const static double T = 2.;                          // Material temperature
 
    // If either boundary indicator is source, those values need to be specified
-   // const static double TL = 0.4;
-   // const static double TR = 0.6;
-   const double psi_source[M] = {0.3273225123, 0.3273225123};
+   const double test_bc = a * c * pow(T, 4) / (4*M_PI);
+   double psi_source[M] = {test_bc, test_bc};
+   // double psi_source[M] = {test_bc, test_bc, test_bc, test_bc};
 
-   // TODO: Change to be cell wise values
-   // TODO: Output plot of initial profiles
-   const static double rho = 1.;                      // Material density
-   const static double kappa = 1.;                    // Absorption opacity
-   const static double T = 1.;                        // Material temperature
 }
 
 #endif // COMPILE_TIME_VALS
